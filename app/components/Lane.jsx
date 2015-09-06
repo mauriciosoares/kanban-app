863665053c31e2b8369a7f55689db8634d034162
Mauricio Soares <m.soares2603@gmail.com>
2015-09-06 11:42:50 -0300

    fix some typos, start lane component

---
+++ app/components/Lane.jsx
@@ -0,0 +1,11 @@
+import AltContainer from 'alt/AltContainer';
+import React from 'react';
+import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
+
+class Lane extends React.Component {
+
+}
+
+export default Lane;
--- app/components/Lanes.jsx
+++ app/components/Lanes.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Lane from './Lane.jsx';
 
 class Lanes extends React.Component {
   render() {
+    const lanes = this.props.items;
+
     return (
       <div className="lanes">
-        lanes should go here
+        { lanes.map(this.rendeLane) }
       </div>
+    );
+  }
+
+  renderLane(lane) {
+    return (
+      <Lane className="lane" key={`lane${lane.id}`} {...lane} />
     );
   }
 }
