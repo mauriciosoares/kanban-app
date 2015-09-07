2ec27948426b3ff3a6d6b0aa81f286ba1f8ed6f8
Mauricio Soares <m.soares2603@gmail.com>
2015-09-07 10:59:49 -0300

    lanes can be edited and removed... also the app has some style

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -4,6 +4,7 @@ import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 import LaneActions from '../actions/LaneActions';
+import Editable from './Editable.jsx';
 
 class Lane extends React.Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class Lane extends React.Component {
 
     this.addNote = this.addNote.bind(this, id);
     this.deleteNote = this.deleteNote.bind(this, id);
+    this.editName = this.editName.bind(this, id);
   }
 
   render() {
@@ -21,7 +23,10 @@ class Lane extends React.Component {
     return(
       <div {...props}>
         <div className="lane-header">
-          <div className="lane-name">{ name }</div>
+          <Editable className="lane-name"
+            value={name}
+            onEdit={this.editName} />
+
           <div className="lane-add-note">
             <button onClick={this.addNote}>+</button>
           </div>
@@ -55,6 +60,14 @@ class Lane extends React.Component {
     console.log('deleting in lane');
     NoteActions.delete(noteId);
     LaneActions.detachFromLane({ laneId, noteId });
+  }
+
+  editName(id, name) {
+    if(name) {
+      LaneActions.update({ id, name });
+    } else {
+      LaneActions.delete(id);
+    }
   }
 }
 
